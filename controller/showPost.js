1dac49c4b92cb9969cbecaeb2fc3c29e5ad36073
blkhrt <riteshkumar3310@gmail.com>
2020-10-09 20:39:42 +0530

    new code pushed for testing

--- controller/showPost.js
+++ controller/showPost.js
@@ -5,30 +5,12 @@ const Post = require("../model/post");
 
 /**
  * @module allPost
- * @param {string} posts - this outputs all post from post schema randomly
+ * @param {String} posts - this outputs all post from post schema randomly
  */
 
 //#region this controller shows all post randomly
 exports.allPost = async (req, res) => {
-  Post.count().exec(async (err, count) => {
-    const random = Math.floor(Math.random() * count);
-    const status = {
-      status: "N",
-    };
-    Post.find({})
-      .skip(random, status)
-      .exec(async (err, result) => {
-        console.log(result);
-        if (err) {
-          return res.json({ Error: "Something Went Wrong", isSuccess: false });
-        } else {
-          return res.json({
-            message: "Showing All Post",
-            isSuccess: true,
-            post: result,
-          });
-        }
-      });
-  });
+  const showPosts = await Post.find({ status: "N" });
+  console.log(showPosts);
 };
 //#endregion
