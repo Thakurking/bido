3051a49a029dd03cfaa96125f007a754f44200f5
blkhrt <riteshkumar3310@gmail.com>
2020-10-24 23:16:05 +0530

    added code for landing page nav section and description page fully working

--- bido-frontend/src/component/navBar.js
+++ bido-frontend/src/component/navBar.js
@@ -1,125 +1,112 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-
-function ElevationScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
-  });
-
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-}
-
-ElevationScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
+import Grid from "@material-ui/core/Grid";
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function ElevateAppBar(props) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <ElevationScroll {...props}>
-        <AppBar color="inherit">
-          <Toolbar>
-            <Typography
-              variant="h3"
-              style={{
-                fontFamily: "monospace",
-              }}
-            >
-              Bido
-            </Typography>
-          </Toolbar>
-          <Toolbar
+      <AppBar
+        color="inherit"
+        position="fixed"
+        style={{
+          padding: 7,
+        }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h2"
             style={{
-              paddingBottom: 10,
+              fontFamily: "monospace",
             }}
           >
-            <ButtonGroup>
-              <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                style={{
-                  fontSize: 20,
-                }}
+            Bido
+          </Typography>
+          <Grid container justify="flex-end">
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              style={{
+                fontSize: 20,
+              }}
+            >
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/login"
               >
-                <Link
-                  style={{ textDecoration: "none", color: "white" }}
-                  to="/login"
-                >
-                  Login
-                </Link>
-              </Button>
-            </ButtonGroup>
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
+                Login
+              </Link>
+            </Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
       <Toolbar />
       <Container>
-        <Box my={10}>
-          <Container>
-            <h1>What is Bido</h1>
-            <h2>
-              Bido is an online bidding platform that gives services to
-              freelancer and user to give a post and bid on four different
-              services -
-            </h2>
-            <ol>
-              <h2>Catering</h2>
-              <h2>Shipping</h2>
-              <h2>Interior Design</h2>
-              <h2>Construction</h2>
-            </ol>
-            <h2>
-              User can post on any of these above categories and freelancers
-              can bid on there post.
-            </h2>
-            <h2>
-              if user accepts there bids then they can contact each other.
-            </h2>
-            <h2>
-              if you want to explore more then please signup:-
-              <Button
-                variant="contained"
-                color="secondary"
-                size="large"
-                style={{
-                  fontSize: 20,
-                }}
-              >
-                <Link
-                  style={{ textDecoration: "none", color: "white" }}
-                  to="/signup"
-                >
-                  Signup
-                </Link>
-              </Button>
-            </h2>
-          </Container>
-        </Box>
+        <h1>What is Bido</h1>
+        <h2>
+          Bido is an online bidding platform that gives services to freelancer
+          and user to post a request on four different services then if any
+          freelancer wanted to fulfil the requirement the the freelancer can bid
+          on that post. We are free so feel free to use.
+        </h2>
+        <h2>
+          Services That We Offer Now.
+        </h2>
+        <ul>
+          <h2>Catering</h2>
+          <h2>Shipping</h2>
+          <h2>Interior Design</h2>
+          <h2>Construction</h2>
+        </ul>
+        <h2>
+          User can post on any of these above categories and freelancers can bid
+          on there post.
+        </h2>
+        <h2>if user accepts there bids then they can contact each other.</h2>
+        <h2>if you want to explore more then please signup</h2>
+        <h2>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{
+              fontSize: 20,
+            }}
+          >
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/signup"
+            >
+              Signup
+            </Link>
+          </Button>
+        </h2>
+        <h1>About</h1>
+        <h2>
+          This application is developed and maintained by Bharat Software
+          Systems
+        </h2>
+        <h2>
+          If you wanted to contribute to our project or create pull request you
+          are welcome..
+        </h2>
+        <h2>
+          Click here to contribute..
+          <Button
+            size="large"
+            href="https://github.com/Thakurking/bido/tree/master"
+          >
+            <GitHubIcon />
+          </Button>
+        </h2>
       </Container>
     </React.Fragment>
   );
