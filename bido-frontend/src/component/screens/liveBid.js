3d7a493549dd14b489c64564a95add6da2cad2ce
blkhrt <riteshkumar3310@gmail.com>
2020-11-15 21:21:57 +0530

    setup live bid page

---
+++ bido-frontend/src/component/screens/liveBid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
+export default function liveBid() {
+    return (
+      <div>
+          
+      </div>
+    );
+}
