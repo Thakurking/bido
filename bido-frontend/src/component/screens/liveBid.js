a9dc9b0e84391a2de77ff6aac309d9b2ccc27436
blkhrt <riteshkumar3310@gmail.com>
2020-11-30 19:14:29 +0530

    made some changes in liveBids page front-end

--- bido-frontend/src/component/screens/liveBid.js
+++ bido-frontend/src/component/screens/liveBid.js
@@ -5,32 +5,23 @@ import { Button, Container } from "react-bootstrap";
 import axios from "axios";
 
 export default function liveBid() {
-  const fetchData = () => {
-    axios
-      .get("/showAllPost", {
-        headers: {
-          user_id: localStorage.getItem("user_id"),
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  // const fetchData = () => {
+  //   axios
+  //     .get("/showAllPost", {
+  //       headers: {
+  //         user_id: localStorage.getItem("user_id"),
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
   return (
     <>
-      {/* <NavBar /> */}
-      <Container>
-        <Button
-          onClick={() => {
-            fetchData();
-          }}
-        >
-          click me!
-        </Button>
-      </Container>
+      <NavBar />
     </>
   );
 }
