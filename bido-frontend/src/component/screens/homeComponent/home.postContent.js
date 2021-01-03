247e9e39eb23045122df134f9f4b3891d3094d7a
blkhrt <riteshkumar3310@gmail.com>
2021-01-03 23:23:37 +0530

    made new page for displaying post

---
+++ bido-frontend/src/component/screens/homeComponent/home.postContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../../App.css";
+import { Container } from "react-bootstrap";
+
+const PostContent = () => {
+    return (
+      <div>
+        <Container className="postContent"></Container>
+      </div>
+    );
+};
+
+export default PostContent;
